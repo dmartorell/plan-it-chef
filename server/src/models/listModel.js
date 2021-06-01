9d0f66c9cac562c17d23f5abd9f3611b8805686d
Daniel Martorell <dani.martorell@gmail.com>
2021-06-01 13:37:33 +0200

    Redo list and recipes models

--- server/src/models/listModel.js
+++ server/src/models/listModel.js
@@ -1,8 +1,21 @@
 const { model, Schema } = require('mongoose');
 
-const userSchema = Schema({
-  nameList: String,
-
+const listSchema = Schema({
+  name: String,
+  date: Date,
+  ingredients: [
+    {
+      name: String,
+      aisle: String,
+      recipeId: [Number],
+      img: String,
+      isActive: Boolean,
+      measures: {
+        us: { amount: Number, unit: String },
+        metric: { amount: Number, unit: String },
+      },
+    },
+  ],
 });
 
-module.exports = model('List', userSchema);
+module.exports = model('List', listSchema);
--- server/src/models/recipeModel.js
+++ server/src/models/recipeModel.js
@@ -1,8 +1,25 @@
 const { model, Schema } = require('mongoose');
 
-const userSchema = Schema({
-  nameRecipe: String,
-
+const recipeSchema = Schema({
+  title: String,
+  sourceUrl: String,
+  summary: String,
+  ingredients: [
+    {
+      name: String,
+      aisle: String,
+      img: String,
+      measures: {
+        us: { amount: Number, unit: String },
+        metric: { amount: Number, unit: String },
+      },
+    },
+  ],
+  instructions: String,
+  preparationMinutes: Number,
+  cookingMinutes: Number,
+  servings: Number,
+  image: String,
 });
 
-module.exports = model('Recipe', userSchema);
+module.exports = model('Recipe', recipeSchema);
