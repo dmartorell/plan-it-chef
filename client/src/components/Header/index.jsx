af773293e98190257e6c1638370d00e4872aaf1b
Daniel Martorell <dani.martorell@gmail.com>
2021-06-02 23:29:17 +0200

    Install iconify and Add user icon and user name to header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import './style.scss';
+import { Icon } from '@iconify/react';
+import userAvatarFilled from '@iconify-icons/carbon/user-avatar-filled';
 import logo from '../../assets/logo-sm.png';
 
 const Header = () => (
@@ -8,6 +11,7 @@ const Header = () => (
       <img className="header__logo" src={logo} alt="logo" />
     </figure>
     <span className="header__user-name">Sylvia</span>
+    <Icon icon={userAvatarFilled} color="#FAFAFA" />
   </header>
 );
 
