e456208af323feb753e4dd118bb27562540047e6
Daniel Martorell <dani.martorell@gmail.com>
2021-06-03 11:12:08 +0200

    Style Changes - Use iconify for user icon - Style recipe url search input

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import './style.scss';
-import { Icon } from '@iconify/react';
-import userAvatarFilled from '@iconify-icons/carbon/user-avatar-filled';
 import logo from '../../assets/logo-sm.png';
 
 const Header = () => (
   <header className="header">
-    <figure>
-      <img className="header__logo" src={logo} alt="logo" />
-    </figure>
-    <span className="header__user-name">Sylvia</span>
-    <Icon icon={userAvatarFilled} color="#FAFAFA" />
+    <div className="header__top">
+      <figure>
+        <img className="logo" src={logo} alt="logo" />
+      </figure>
+      <span className="user-name">Sylvia</span>
+      <span className="iconify user-icon" data-icon="carbon:user-avatar-filled" data-inline="false" />
+    </div>
+    <div className="header__bottom">
+      <form>
+        <input className="form-control" type="text" size="38" placeholder="Paste recipe URL..." id="" name="" />
+        <input className="form-btn" type="submit" />
+      </form>
+    </div>
   </header>
 );
 
