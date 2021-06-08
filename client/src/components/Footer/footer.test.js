ac2a2517bacd6ced278cd8ddf1dba5c3e883713a
Daniel Martorell <dani.martorell@gmail.com>
2021-06-08 19:22:25 +0200

    Add footer testing

--- client/src/components/Footer/footer.test.js
+++ client/src/components/Footer/footer.test.js
@@ -1,15 +1,26 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { render, screen } from '../../utils/test-utils';
-import Header from './index';
+import { fireEvent, render, screen } from '../../utils/test-utils';
+import Footer from './index';
+import { loadRecipes } from '../../redux/actions/actionCreators';
 
-describe('Given a Header component', () => {
+jest.mock('../../redux/actions/actionCreators');
+
+describe('Given a Footer component', () => {
   describe('When is rendered', () => {
-    test('Then the app small logo should be in the document', () => {
-      render(
-        <Header />,
-      );
-      expect(screen.getByTestId('logo-sm')).toBeInTheDocument();
+    test('Then \'Planner\' should be in the document', () => {
+      const initialState = { recipes: [] };
+      loadRecipes.mockReturnValue({ type: 'LOAD_RECIPES', recipes: [] });
+      render(<Footer />, { initialState });
+      expect(screen.getByText('Planner')).toBeInTheDocument();
+    });
+  });
+  describe('When is rendered', () => {
+    test('Then \'Planner\' should be in the document', () => {
+      loadRecipes.mockReturnValue({ type: 'LOAD_RECIPES', recipes: [] });
+      render(<Footer />);
+      fireEvent.click(screen.getByTestId('recipes-btn'));
+      expect(loadRecipes).toHaveBeenCalled();
     });
   });
 });
