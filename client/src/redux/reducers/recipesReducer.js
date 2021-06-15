e98dbb3e32a29a049191097e262079c0e326ec96
Daniel Martorell <dani.martorell@gmail.com>
2021-06-15 11:14:59 +0200

    Modify recipesReducer to Add new search recipe to user list

--- client/src/redux/reducers/recipesReducer.js
+++ client/src/redux/reducers/recipesReducer.js
@@ -1,9 +1,13 @@
 import actionTypes from '../actions/actionTypes';
 
 function recipesReducer(recipes = [], action) {
+  // eslint-disable-next-line no-debugger
+  debugger;
   switch (action.type) {
     case actionTypes.LOAD_RECIPES:
       return action.recipes;
+    case actionTypes.GET_API_RECIPE:
+      return [...recipes, action.recipe];
     default:
       return recipes;
   }
